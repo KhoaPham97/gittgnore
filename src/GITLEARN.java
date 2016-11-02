fb8db5fa3949832820fbba559217d5fff606b28c
KhoaPham <phamdangkhoa0403@gmail.com>
2016-11-02 17:07:59 +0700

    Init project

---
+++ src/GITLEARN.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author KhoaPham
+ *
+ */
+public class GITLEARN extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GITLEARN mainWindow =new GITLEARN();
+		mainWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		mainWindow.setVisible(true);
+		mainWindow.toBack();
+		
+		
+	}
+
+}
